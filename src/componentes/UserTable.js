c3c95b81cd45a5dfc0d3c191f188d4d7be8ee0ba
n1c0145 <nicolas.lozalml@gmail.com>
2022-06-23 10:31:40 -0500

    READ crud finish

---
+++ src/componentes/UserTable.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+
+function UserTable(props) {
+
+console.log(props.users);
+
+    return (
+
+        <table>
+
+            <thead>
+
+                <tr>
+
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Edad</th>
+                    <th>Correo</th>
+                    <th>Direccion</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {props.users.map(user=>(
+          <tr key={user.id}>
+          <td>{user.nombre}</td>
+          <td>{user.apellido}</td>
+          <td>{user.edad}</td>
+          <td>{user.correo}</td>
+          <td>{user.direccion}</td>
+          <td>
+              <button>Editar</button>
+              <button>Eliminar</button>
+          </td>
+
+
+      </tr>
+                ))}
+      
+            </tbody>
+        </table>
+    )
+
+};
+
+export default UserTable;
--- src/paginas/Datos.js
+++ src/paginas/Datos.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UserTable from '../componentes/UserTable.js';
 import '../estilos/datos.css';
+import { v4 as uuidv4 } from "uuid";
 
 function Datos() {
-return(
-<div>CRUD</div>
-);
+
+    const usersData = [
+
+        {
+            id:uuidv4(),
+            nombre: "Nicolas",
+            apellido: "Loza",
+            edad: 23,
+            correo: "nicola.loza@gmail.com",
+            direccion: "Tumbaco"
+        },
+        {
+            id:uuidv4(),
+            nombre: "Juan",
+            apellido: "Perez",
+            edad: 35,
+            correo: "juanp@gmail.com",
+            direccion: "Pifo"
+        },
+        {
+            id:uuidv4(),
+            nombre: "Diana",
+            apellido: "Carolina",
+            edad: 18,
+            correo: "dcaro@gmail.com",
+            direccion: "Cumbaya"
+        }
+    ]
+
+    const [users, setUsers] = useState(usersData)
+
+
+    return (
+        <div className='container'>
+            <h1>CRUD HOOKS</h1>
+            <div><h2>Add user</h2></div>
+            <div><h2>View User</h2></div>
+            <UserTable users={users}/>
+        </div>
+    );
 }
 
 export default Datos;
