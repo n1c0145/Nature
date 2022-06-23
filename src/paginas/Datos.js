775afd6a3e7ac370d04e6dc3a21914ecab4f5cbf
n1c0145 <nicolas.lozalml@gmail.com>
2022-06-23 09:36:39 -0500

    Refactor failed CRUD

--- src/paginas/Datos.js
+++ src/paginas/Datos.js
@@ -1,46 +1,10 @@
-import React,{useState} from 'react';
+import React from 'react';
 import '../estilos/datos.css';
-import Formulario from "../componentes/Formulario.js";
-import Tabla from "../componentes/Tabla.js";
-
-const initialDb = [
-
-{
-  id:1,
-  nombre:"Nicolas",
-  apellido:"Loza",
-  edad:23,
-  telefono:998790522,
-  direccion:"Tumbaco"
-},{
-  id:2,
-  nombre:"Juan",
-  apellido:"Perez",
-  edad:33,
-  telefono:996785322,
-  direccion:"Pifo"
-},
-{
-  id:3,
-  nombre:"Carla",
-  apellido:"Ayala",
-  edad:18,
-  telefono:996738112,
-  direccion:"Cumbaya"
-}
-]
-
 
 function Datos() {
-   const[db,setDb]=useState(initialDb);
-
-  return (
-    <div>
-  <div>Tabla de datos</div>
-  <Formulario />
- <Tabla data={db}/>
-    </div>
-  );
+return(
+<div>CRUD</div>
+);
 }
 
 export default Datos;
