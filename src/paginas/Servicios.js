f5b86370a78d7feef9567de686d984c3c137858d
n1c0145 <nicolas.lozalml@gmail.com>
2022-06-24 14:58:46 -0500

    services inicio finish

--- src/paginas/Servicios.js
+++ src/paginas/Servicios.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import '../estilos/servicios.css';
-
+import img1 from "../assets/img1.png";
 function Servicios() {
     return (
 
-        <div>
-            <h1>Servicios</h1>
-        
-        </div>
+        <div className='servicios text-center'>
+            <div className='servicios-tittle text-center fs-3 mt-5 fw-bolder'>FORESTRY SERVICES</div>
+            <div className='row'>
+                <div className='col servicios-box mt-5'>
+                    <img src={img1}/>
+                </div>
+                <div className='col servicios-box mt-5'>
+                    <img src={img1}/>
+                </div>
+                <div className='col servicios-box mt-5'>
+                    <img src={img1}/>
+                </div>
+
+            </div>
+            <div className='row'>
+            <div className='col servicios-box mt-5'>
+                    <img src={img1}/>
+                </div>
+                <div className='col servicios-box mt-5'>
+                    <img src={img1}/>
+                </div>
+                <div className='col servicios-box mt-5'>
+                    <img src={img1}/>
+                </div>
+       
+            </div>
+
+        </div> 
     )
 
 };
