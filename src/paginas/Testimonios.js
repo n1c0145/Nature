20382a92ac1b4ff0f26b5da5d0e9c81b21f310ba
n1c0145 <nicolas.lozalml@gmail.com>
2022-06-24 17:24:57 -0500

    testimonios finish

--- src/paginas/Testimonios.js
+++ src/paginas/Testimonios.js
@@ -9,11 +9,11 @@ function Testimonios() {
             <div className='fs-5 mt-1 font-weight-normal text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec.</div>
             <div className='testimonios-img-box mx-auto'>
                 <div className='testimonios-text-box'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nibh lectus, pharetra ut tempus vitae, suscipit sagittis dolor. Nullam scelerisque elit vitae tempus maximus. Duis.</p>
-                    <div>Lorem ipsum dolor sit amet, consectetur</div>
+                    <p className='text pe-5 px-5'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam nibh lectus, pharetra ut tempus vitae, suscipit sagittis dolor. Nullam scelerisque elit vitae tempus maximus. Duis."</p>
+                    <div className='text mt-5 fw-bold'>Lorem ipsum dolor sit amet, consectetur</div>
 
                 </div>
-            </div>
+            </div> 
         </div>
 
     )
